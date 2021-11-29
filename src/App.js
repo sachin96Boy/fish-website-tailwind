218d2df73088e5a0d10b767c642471b3937e7b34
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-11-29 13:09:07 +0530

    upate: add Navbar and other components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-red-500">Hello world</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
